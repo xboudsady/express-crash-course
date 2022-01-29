afe2406320dfcdb2e4ea9142cb6c595802e4d109
xboudsady <boudsadyx@gmail.com>
2022-01-28 19:07:45 -0800

    Added static JSON Date for testing

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ const path = require("path");
 
 const app = express();
 
+//Static JSON data for testing
+const members = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john@gmail.com",
+        status: "active",
+    },
+    {
+        id: 2,
+        name: "Bob Williams",
+        email: "bob@gmail.com",
+        status: "inactive",
+    },
+    {
+        id: 3,
+        name: "Shannon Jackson",
+        email: "shannon@gmail.com",
+        status: "status",
+    },
+];
+
+app.get("/api/members", (req, res) => {});
+
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
