9a0d28de0dc6cae3de87c350331931b3d43b9422
xboudsady <boudsadyx@gmail.com>
2022-01-28 18:47:30 -0800

    Added static folder, added about.html, added css folder, and style.css

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 8000;
 
