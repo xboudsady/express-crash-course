e72d389d4a2ac304f41225f483ed57a1f4e23751
xboudsady <boudsadyx@gmail.com>
2022-01-28 22:48:05 -0800

    Created separted logger folder for middleware, moved logger.js

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const path = require("path");
-const moment = require("moment");
+const logger = require("./middleware/logger");
 const members = require("./Members");
 
 const app = express();
-
-const logger = (req, res, next) => {
-    console.log(
-        `${req.protocol}://${req.get("host")}${
-            req.originalUrl
-        }: ${moment().format()}`,
-    );
-    next();
-};
 
 // Init middleware
 app.use(logger);
