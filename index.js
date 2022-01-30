b979f81f9cba02877969d0f0d2d61bb57c3c4713
xboudsady <boudsadyx@gmail.com>
2022-01-29 19:57:00 -0800

    Updated index file to load members json data file

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
+const { title } = require("process");
+const members = require("./Members");
 
 const app = express();
 
 // Init middleware
 // app.use(logger);
 
+// Handlebars Middleware
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Homepage Route
+app.get("/", (req, res) =>
+    res.render("index", {
+        title: "Member App",
+        members,
+    }),
+);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
