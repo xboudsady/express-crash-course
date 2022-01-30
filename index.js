1a278ec842f4e73fb6d6826e45ad88891f319be0
xboudsady <boudsadyx@gmail.com>
2022-01-29 17:28:17 -0800

    Created new files members.js for routing paths

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
 const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
-const members = require("./Members");
 
 const app = express();
 
 // Init middleware
 // app.use(logger);
 
-// Gets All members
-app.get("/api/members", (req, res) => res.json(members));
-
-// Get Single Member
-app.get("/api/members/:id", (req, res) => {
-    const found = members.some(
-        (member) => member.id === parseInt(req.params.id),
-    );
-
-    if (found) {
-        res.json(
-            members.filter((member) => member.id === parseInt(req.params.id)),
-        );
-    } else {
-        res.status(400).json({
-            msg: `No member with the id of ${req.params.id}`,
-        });
-    }
-});
-
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 8000;
 
